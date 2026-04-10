e6233322bb15322ae591818090671e88daff236f
cole cueva <cole8cueva@gmail.com>
2026-04-10 17:11:32 -0500

    Delete script.js

--- script.js
+++
@@ -1,6 +0,0 @@
-// Function to display the current time
-function displayTime() {
-    const currentTime = new Date();
-    const timeString = currentTime.toLocaleTimeString();
-    document.getElementById('current-time').innerText = "Current Time: " + timeString;
-}
